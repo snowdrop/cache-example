f2fcbdcf9ad14e900c7a29ae349bd5c76b2649fc
Jose <josecarvajalhilario@gmail.com>
2021-12-13 13:05:00 +0100

    Change startup conditions before running tests

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
+source scripts/waitFor.sh
 
 # launch the cache service
 oc create -f .openshiftio/cache.yml
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"deploy\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "cache-server" "app") -eq 1 ]] ; then
+  echo "Cache server failed to deploy. Aborting"
+  exit 1
+fi
 
 # 1.- Deploy Cute Name Service
 ./mvnw -s .github/mvn-settings.xml clean verify -pl cute-name-service -Popenshift -Ddekorate.deploy=true
-
-# wait for the app to stand up
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "spring-boot-cache-cutename" "app.kubernetes.io/name") -eq 1 ]] ; then
+  echo "Cute name service failed to deploy. Aborting"
+  exit 1
+fi
 
 # 2.- Deploy Greeting Service
 ./mvnw -s .github/mvn-settings.xml clean verify -pl greeting-service -Popenshift -Ddekorate.deploy=true
-
-# wait for the app to stand up
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "spring-boot-cache-greeting" "app.kubernetes.io/name") -eq 1 ]] ; then
+  echo "Greeting name service failed to deploy. Aborting"
+  exit 1
+fi
 
 # 3.- Run OpenShift Tests
 ./mvnw -s .github/mvn-settings.xml verify -pl tests -Popenshift-it
