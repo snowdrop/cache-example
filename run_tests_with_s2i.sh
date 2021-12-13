af10aac7566a1a01bd9e6d5ea46f7a2b454f945b
Jose <josecarvajalhilario@gmail.com>
2021-12-13 13:05:00 +0100

    Change startup conditions before running tests

--- run_tests_with_s2i.sh
+++ run_tests_with_s2i.sh
@@ -2,19 +2,24 @@
 SOURCE_REPOSITORY_URL=${1:-https://github.com/snowdrop/cache-example}
 SOURCE_REPOSITORY_REF=${2:-sb-2.4.x}
 
+source scripts/waitFor.sh
+
 # launch the cache service
 oc create -f .openshiftio/cache.yml
-sleep 30 # needed in order to bypass the 'Pending' state
-timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"deploy\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+if [[ $(waitFor "cache-server" "app") -eq 1 ]] ; then
+  echo "Cache server failed to deploy. Aborting"
+  exit 1
+fi
 
 declare -Ar moduleMapping=( ["greeting-service"]="spring-boot-cache-greeting" ["cute-name-service"]="spring-boot-cache-cutename" )
 for module in "${!moduleMapping[@]}"
 do
   oc create -f ${module}/.openshiftio/application.yaml
   oc new-app --template=${moduleMapping[$module]} -p SOURCE_REPOSITORY_URL=$SOURCE_REPOSITORY_URL -p SOURCE_REPOSITORY_REF=$SOURCE_REPOSITORY_REF -p SOURCE_REPOSITORY_DIR=${module}
-  sleep 60 # needed in order to bypass the 'Pending' state
-  # wait for the app to stand up
-  timeout 300s bash -c 'while [[ $(oc get pod -o json | jq  ".items[] | select(.metadata.name | contains(\"build\"))  | .status  " | jq -rs "sort_by(.startTme) | last | .phase") == "Running" ]]; do sleep 20; done; echo ""'
+  if [[ $(waitFor ${moduleMapping[$module]} "app") -eq 1 ]] ; then
+    echo "${module} service failed to deploy. Aborting"
+    exit 1
+  fi
 done
 
 # launch the tests without deploying the application
